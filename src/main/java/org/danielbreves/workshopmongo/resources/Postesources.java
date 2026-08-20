4c61cb6d5ec774d74d0055b4f3c4545b1e84eb87
Daniel-Brevess <danielbreves1.20@gmail.com>
2026-08-20 09:28:22 -0300

    endpoint for get posts by id

---
+++ src/main/java/org/danielbreves/workshopmongo/resources/Postesources.java
@@ -0,0 +1,33 @@
+package org.danielbreves.workshopmongo.resources;
+
+import org.danielbreves.workshopmongo.domain.Post;
+import org.danielbreves.workshopmongo.domain.User;
+import org.danielbreves.workshopmongo.dto.UserDTO;
+import org.danielbreves.workshopmongo.service.PostService;
+import org.danielbreves.workshopmongo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value="/posts")
+public class Postesources {
+
+    @Autowired
+    private PostService postService;
+
+
+    @GetMapping(value="/{id}")
+    public ResponseEntity <Post> findById(@PathVariable("id") String id) {
+
+        Post obj = postService.findById(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+    
+}
---
+++ src/main/java/org/danielbreves/workshopmongo/service/PostService.java
@@ -0,0 +1,27 @@
+package org.danielbreves.workshopmongo.service;
+
+import org.danielbreves.workshopmongo.domain.Post;
+import org.danielbreves.workshopmongo.domain.User;
+import org.danielbreves.workshopmongo.dto.UserDTO;
+import org.danielbreves.workshopmongo.repository.PostRepository;
+import org.danielbreves.workshopmongo.repository.UserRepository;
+import org.danielbreves.workshopmongo.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+
+    public Post findById(String id) {
+        return postRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("Post não encontrado: " + id));
+    }
+
+
+}
